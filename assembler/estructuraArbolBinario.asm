ec646bf2a601a759f2ce63e90a7adeb601be846d
Giuliano1994 <gomesdasilva.giuliano@gmail.com>
2023-10-07 21:04:31 -0300

    correccion de errores al crear un arbol y mostrarlo

--- assembler/estructuraArbolBinario.asm
+++ assembler/estructuraArbolBinario.asm
@@ -1,4 +1,3 @@
-; defino la estructura de arbol, tiene 3 campos de 8 bytes
 
 section .data
 struc Arbol
@@ -7,43 +6,36 @@ struc Arbol
   .right resq 1
 endstruc
 
-
-
-; funcion para crear arbol, toma 3 argumentos de rdi, rsi y rdx
-
 section .text
+extern malloc, free
 global createArbol
 
-createArbol:
+createArbol_asm:
   ; Argumentos:
   ; rdi: Valor del nodo
   ; rsi: Puntero al nodo izquierdo
   ; rdx: Puntero al nodo derecho
-  
-  ; Reserva espacio para la estructura Arbol en la pila
-  push rdi
-  push rsi
-  push rdx
 
- 
+  ; Calcula el tamaño necesario para la estructura Arbol
+  mov rax, 24
+
+  ; Llama a malloc para asignar memoria
+  call malloc
+
+  ; Comprueba si malloc tuvo éxito (rax contiene el puntero si es exitoso)
+  test rax, rax
+  jz .malloc_failed
+
   ; Configura el valor del nodo
-  mov rax, rdi  ; rax = valor
+  mov qword [rax + Arbol.value], rdi
 
-  ; HAGO VIOLACIONES DE SEGMENTO AL AGREGAR UN SEGUNDO NODO AL ARBOL, AL ESTABLECER ARBOL.RIGHT Y LEFT EN 0 ESTOY INICIALIZANDOLOS
-  mov qword [rsp + Arbol.left], 0
-  mov qword [rsp + Arbol.right], 0
+ ; Configura el puntero izquierdo a NULL
+  mov qword [rax + Arbol.left], 0
 
-  ; el regustro rsp es un puntero a la pila, apunta al tope de la pila (stack pointer) 
-  ; Configura el puntero izquierdo
-  mov qword [rsp + Arbol.left], rsi
+  ; Configura el puntero derecho a NULL
+  mov qword [rax + Arbol.right], 0
 
-  ; Configura el puntero derecho
-  mov qword [rsp + Arbol.right], rdx
-
-  ; Retorna la dirección del nodo creado (en rsp)
-  
-  ; Restaura la pila
-  pop rdx
-  pop rsi
-  pop rdi
   ret
+
+.malloc_failed:
+  ; Maneja la falla de malloc, puedes agregar código aquí para manejar errores
